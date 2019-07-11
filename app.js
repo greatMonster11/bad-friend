17020090eafd749a06f9b6e88561e6e85ed40f7d
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-07-11 16:03:43 +0700

    read out loud

--- app.js
+++ app.js
@@ -15,9 +15,20 @@ recognition.onresult = function(event) {
 
   const transcript = event.results[current][0].transcript;
   content.textContent = transcript;
+  readOutLoud(transcript);
 };
 
 //add the listener to the button
 btn.addEventListener("click", () => {
   recognition.start();
 });
+
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = message;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
+}
